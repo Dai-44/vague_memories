e60c40974a5258076580d51b925777aa3d7a1c74
Dai <kobayashi.daigo3@gmail.com>
2023-08-08 15:35:04 +0900

    #4 require_loginの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  add_flash_types :success, :danger
+  add_flash_types :success, :danger, :warning
+  before_action :require_login
+
+  private
+  def not_authenticated
+    flash[:warning] = "権限がありません"
+    redirect_to login_path
+  end
 end
