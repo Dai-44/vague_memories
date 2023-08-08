57eacbd0ce33dffe2f1df80b30a484d65cbb9797
Dai <kobayashi.daigo3@gmail.com>
2023-08-08 20:58:13 +0900

    #12 postsテーブル作成, バリデーション定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_08_041150) do
+ActiveRecord::Schema.define(version: 2023_08_08_115317) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
