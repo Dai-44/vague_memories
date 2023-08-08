d9cb9752c3216871fe3ef7c36469ba2494f5c743
Dai <kobayashi.daigo3@gmail.com>
2023-08-08 14:42:23 +0900

    #4 ログイン機能の実装, 挙動確認

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,14 @@
 class UserSessionsController < ApplicationController
-  def new
+  
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, success: "ログインに成功しました"
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   
   root "static_pages#top"
   get 'login', to: "user_sessions#new"
+  post 'login', to: "user_sessions#create"
   resources :users, only: %i[new create]
 end
