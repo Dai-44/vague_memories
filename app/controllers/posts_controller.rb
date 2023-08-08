6566df774fe2cc8ab2bcb10bc6213bdaea770ea5
Dai <kobayashi.daigo3@gmail.com>
2023-08-08 21:50:03 +0900

    #12 掲示板一覧画面の実装, 挙動確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def new
+    @post = Post.new
   end
 
   def show
   end
 
-  def edit
+  def edit 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 end
