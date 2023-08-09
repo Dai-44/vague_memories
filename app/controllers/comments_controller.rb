97b3b55cae81c90fee3fd95b26e2fb0ef3c5704a
Dai <kobayashi.daigo3@gmail.com>
2023-08-09 12:27:23 +0900

    #15 コメント作成機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,17 @@
 class CommentsController < ApplicationController
-  
-  def index
-
-  end
-
-  def new
-    @comment = Comment.new
-  end
 
   def create
-
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
   end
 
   def destroy
 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(post_id: params[:post_id])
   end
 end
