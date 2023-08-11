513898a613e39e14f1398de66119ba4089444e52
Dai <kobayashi.daigo3@gmail.com>
2023-08-11 10:38:18 +0900

    #32 ブックマーク処理に関する各種アクション定義(保存漏れを含めた)

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,8 +2,11 @@ class BookmarksController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    current_user.bookmark(@post)
   end
 
   def destroy
+    @post = current_user.bookmarks.find(params[:id]).post
+    @post = current_user.unbookmark(@post)
   end
 end
