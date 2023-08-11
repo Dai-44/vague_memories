d866f3e66ca3738215b1419380795a5c57433828
Dai <kobayashi.daigo3@gmail.com>
2023-08-11 10:25:24 +0900

    #32 posts#bookmarkのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: "user_sessions#destroy"
   resources :users, only: %i[new create]
   resources :posts do
+    get 'bookmark', on: :collection
     resources :comments, only: %i[create destroy], shallow: true
   end
 
